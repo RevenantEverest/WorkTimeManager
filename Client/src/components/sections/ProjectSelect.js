26180d9c68d53691520dd06c3f42a3dadee460b3
Stefan <Stefischer@mail.com>
2021-06-02 12:04:56 -0400

    Added Dropdown select for projects

---
+++ Client/src/components/sections/ProjectSelect.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import '../css/ProjectSelect.css';
+import {
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem
+} from 'mdbreact';
+
+import projectServices from '../../services/projectServices';
+
+function ProjectSelect() {
+
+    const [projects, setProjects] = useState([]);
+    const [chosenProject, setChosenProject] = useState(null);
+
+    useEffect(() => {
+        getProjects();
+    }, []);
+
+    const getProjects = () => {
+        projectServices.getByUserId(1)
+        .then(results => setProjects(results.data.data))
+        .catch(err => console.error(err));
+    };
+
+    const handleChange = (project) => {
+        setChosenProject(project);
+    };
+
+    const renderSelect = () => {
+        let Projects = projects.map((el, idx) => {
+            return(
+                <MDBDropdownItem key={idx} onClick={() => handleChange(el)}>
+                    <span className="dropdown-span">{el.name}</span>
+                </MDBDropdownItem>
+            );
+        });
+        
+        return(
+            <MDBDropdown onChange={handleChange}>
+            <MDBDropdownToggle caret color="elegant" size="sm">
+                {chosenProject ? handleProjectName() : 'Select Project' }
+            </MDBDropdownToggle>
+            <MDBDropdownMenu>
+                {Projects}
+            </MDBDropdownMenu>
+            </MDBDropdown>
+        );
+    };
+
+    const handleProjectName = () => {
+        let project = chosenProject;
+        let projectName = project.name;
+        if(project.name.split("").length > 15) {
+            projectName = "";
+            for(let i = 0; i < project.name.split("").length; i++) {
+                if(i < 15) projectName += project.name.split("")[i];
+            }
+            projectName += "..."
+        }
+        return projectName;
+    };
+
+    return(
+        <div className="ProjectSelect">
+            {projects.length > 0 ? renderSelect() : ''}
+        </div>
+    );
+};
+
+export default ProjectSelect;
